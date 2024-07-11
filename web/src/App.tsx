24b909892dd8d25a46509418b4c452d2a14f92b1
Marcos Merlini <merlinimarcos@gmail.com>
2024-07-11 16:06:17 -0300

    eh isso pessoal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,55 +3,60 @@ import "./App.css";
 import { ForecastCard } from "./components/ForecastCard";
 import axios from "axios";
 
-const mockForecast = {
-  name: "Chapecó",
-  country: "Brasil",
-  region: "Santa Catarina",
-  lat: "-27.083",
-  lon: "-52.983",
-  timezone_id: "America/Sao_Paulo",
-  localtime: "2024-06-14 16:52",
-  localtime_epoch: 1718383920,
-  utc_offset: "-3.0",
-  observation_time: "07:52 PM",
-  temperature: 27,
-  weather_code: 113,
-  weather_icons: [
-    "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png",
-    "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png",
-  ],
-  weather_descriptions: ["Sunny", "Clear"],
-  wind_speed: 24,
-  wind_degree: 360,
-  wind_dir: "N",
-  pressure: 1013,
-  precip: 0,
-  humidity: 51,
-  cloudcover: 0,
-  feelslike: 28,
-  uv_index: 7,
-  visibility: 10,
-  is_day: "yes",
-};
-
 function App() {
   const [cep, setCep] = useState("");
-  const [forecast, setForecast] = useState(mockForecast);
+  const [forecast, setForecast] = useState(null);
 
+  /**
+   * Method to validate the CEP format
+   *
+   * @param cep
+   */
   function onCepChange(cep: string) {
     if (/^[0-9]+$/.test(cep) || cep.length == 0) {
       setCep(cep);
     }
   }
 
+  /**
+   * Call the API with the CEP informed and return the name of the city
+   *
+   * @returns object
+   */
+  async function getCity() {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_SERVER}/city/${cep}`
+    );
+
+    return response.data;
+  }
+
+  /**
+   * Call the API with the city and return the forecast for that
+   *
+   * @param city
+   * @returns object
+   */
+  async function getForecast(city: string) {
+    const forecast = await axios.get(
+      `${import.meta.env.VITE_API_SERVER}/weather/${city}`
+    );
+
+    return forecast.data;
+  }
+
   async function searchForecast() {
-    // const response = await axios.get(
-    //   `${import.meta.env.VITE_API_SERVER}/city/${cep}`
-    // );
-    // console.log(response);
-    // if (cep.length === 9) {
-    //   console.log(import.meta.env.API_SERVER);
-    // }
+    if (cep.length === 9) {
+      console.log("CEP inválido!");
+    }
+
+    try {
+      const city = await getCity();
+      const forecast = await getForecast(city);
+      setForecast(forecast);
+    } catch (error) {
+      throw new Error("Erro ao consultar o clima para a cidade informada!");
+    }
   }
 
   return (
@@ -68,7 +73,10 @@ function App() {
           />
           <button onClick={searchForecast}>Buscar</button>
         </span>
-        <ForecastCard {...forecast} />
+        {forecast ? (
+          // @ts-expect-error pq eu quero
+          <ForecastCard {...forecast} />
+        ) : ''}
       </div>
     </>
   );
